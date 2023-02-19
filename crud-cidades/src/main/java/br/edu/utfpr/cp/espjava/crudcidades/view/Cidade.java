33991ed52229c78eef1513d5ee239cb1bfcb7dd9
wellington vieira <wellington.vieira@fatec.sp.gov.br>
2023-02-19 12:54:05 -0300

    criando pagina dinamica

---
+++ crud-cidades/src/main/java/br/edu/utfpr/cp/espjava/crudcidades/view/Cidade.java
@@ -0,0 +1,20 @@
+package br.edu.utfpr.cp.espjava.crudcidades.view;
+
+public final class Cidade {
+
+    private final String nome;
+    private final String estado;
+
+    public Cidade(final String nome, final String estado) {
+        this.nome = nome;
+        this.estado = estado;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+}
--- crud-cidades/src/main/java/br/edu/utfpr/cp/espjava/crudcidades/view/CidadeController.java
+++ crud-cidades/src/main/java/br/edu/utfpr/cp/espjava/crudcidades/view/CidadeController.java
@@ -1,13 +1,24 @@
 package br.edu.utfpr.cp.espjava.crudcidades.view;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Set;
 
 @Controller
 public class CidadeController {
 
     @GetMapping("/")
-    public String listar() {
-        return "crud.html";
+    public String listar(Model memoria) {
+
+        var cidades = Set.of(
+                new Cidade("Cornélio Procópio", "PR"),
+                new Cidade("Assis", "SP"),
+                new Cidade("Itajaí", "SC")
+        );
+
+        memoria.addAttribute("listaCidades", cidades);
+        return "/crud";
     }
 }
